b873d18dc4ceef85475a163fe134154c2c7b1a96
Corentin Derbré <LoeizD@users.noreply.github.com>
2018-11-19 12:08:25 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -13,7 +13,8 @@
 // as `MONGO_URI`. Connect to the database using `mongoose.connect(<Your URI>)`
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
+// mongoose.connect(process.env.MONGO_URI);
+console.log(process.env.MONGO_URI);
 
 /** # SCHEMAS and MODELS #
 /*  ====================== */
